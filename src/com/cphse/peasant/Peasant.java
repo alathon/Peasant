04df579484587fa52e16f8be02006b15d8c1c091
Martin Grunbaum <martin@itsolveonline.net>
2011-07-29 03:57:50 +0200

    Added mail order test functio

--- src/com/cphse/peasant/Peasant.java
+++ src/com/cphse/peasant/Peasant.java
@@ -1,7 +1,21 @@
 package com.cphse.peasant;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.cphse.mailman.connection.MailConnectionDetails;
+import com.cphse.orders.MailAccountOrder;
+import com.cphse.orders.MailOrderProcessor;
+import com.cphse.queue.OrderSubmitter;
+
 public final class Peasant {
-	public static void main(String[] args) {
+	public static void testMailOrder()  throws FileNotFoundException, IOException {
+		MailOrderProcessor processor = new MailOrderProcessor(5);
+		processor.start();
+		MailAccountOrder order = new MailAccountOrder(new MailConnectionDetails("./testAccount.props"));
+		OrderSubmitter.submitOrder(order);
+	}
+	public static void main(String[] args){
 		Mob.getMob().joinMob();
 	}
 }
